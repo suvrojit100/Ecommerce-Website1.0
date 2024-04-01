6585a1e5b5bf2f4b2e267602c10a95437da28692
Suvrojit <csuvrojit.2002@gmail.com>
2024-04-01 18:19:19 +0530

    ji

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { NavLink, Link } from "react-router-dom";
+// import { GiShoppingBag } from "react-icons/gi";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header comp</h1>
+    </div>
+  );
+};
+
+export default Header;
