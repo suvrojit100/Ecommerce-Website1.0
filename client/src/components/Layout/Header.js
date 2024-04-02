75dfb25eefc359580817e555c914c1ccac57ad79
Suvrojit <csuvrojit.2002@gmail.com>
2024-04-03 01:25:43 +0530

    Initial commit

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,114 +1,21 @@
-// import React from "react";
-// import { NavLink, Link } from "react-router-dom";
-// import { GiShoppingBag } from "react-icons/gi";
-// import { useAuth } from "../../context/auth";
-
-// const Header = () => {
-//   const [auth, setAuth] = useAuth();
-//   const handelLogout = () => {
-//     setAuth({
-//       ...auth,
-//       useer: null,
-//       token: "",
-//     });
-//     localStorage.removeItem("auth");
-//   };
-//   return (
-//     <>
-//       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//         <div className="container-fluid">
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarTogglerDemo01"
-//             aria-controls="navbarTogglerDemo01"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon" />
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-//             <Link to="/" className="navbar-brand">
-//               <GiShoppingBag /> Ecommerce App
-//             </Link>
-//             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <NavLink to="/" className="nav-link ">
-//                   Home
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <NavLink to="/category" className="nav-link ">
-//                   Category
-//                 </NavLink>
-//               </li>
-//               {!auth.user ? (
-//                 <>
-//                   <li className="nav-item">
-//                     <NavLink to="/register" className="nav-link">
-//                       Register
-//                     </NavLink>
-//                   </li>
-//                   <li className="nav-item">
-//                     <NavLink to="/login" className="nav-link">
-//                       Login
-//                     </NavLink>
-//                   </li>
-//                 </>
-//               ) : (
-//                 <>
-//                   <li className="nav-item">
-//                     <NavLink
-//                       onClick={handelLogout}
-//                       to="/login"
-//                       className="nav-link"
-//                     >
-//                       Logout
-//                     </NavLink>
-//                   </li>
-//                 </>
-//               )}
-//               <li className="nav-item">
-//                 <NavLink to="/cart" className="nav-link">
-//                   Cart (0)
-//                 </NavLink>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// };
-
-// export default Header;
-
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { GiShoppingBag } from "react-icons/gi";
 import { useAuth } from "../../context/auth";
-import toast from "react-hot-toast";
-import SearchInput from "../Form/SearchInput";
-import useCategory from "../../hooks/useCategory";
-import { useCart } from "../../context/cart";
-import { Badge } from "antd";
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
-  const [cart] = useCart();
-  const categories = useCategory();
-  const handleLogout = () => {
+  const handelLogout = () => {
     setAuth({
       ...auth,
-      user: null,
+      useer: null,
       token: "",
     });
     localStorage.removeItem("auth");
-    toast.success("Logout Successfully");
   };
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <button
             className="navbar-toggler"
@@ -123,43 +30,20 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              🛒 Ecommerce App
+              <GiShoppingBag /> Ecommerce App
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <SearchInput />
               <li className="nav-item">
                 <NavLink to="/" className="nav-link ">
                   Home
                 </NavLink>
               </li>
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to={"/categories"}
-                  data-bs-toggle="dropdown"
-                >
-                  Categories
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to={"/categories"}>
-                      All Categories
-                    </Link>
-                  </li>
-                  {categories?.map((c) => (
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        to={`/category/${c.slug}`}
-                      >
-                        {c.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+              <li className="nav-item">
+                <NavLink to="/category" className="nav-link ">
+                  Category
+                </NavLink>
               </li>
-
-              {!auth?.user ? (
+              {!auth.user ? (
                 <>
                   <li className="nav-item">
                     <NavLink to="/register" className="nav-link">
@@ -174,45 +58,20 @@ const Header = () => {
                 </>
               ) : (
                 <>
-                  <li className="nav-item dropdown">
+                  <li className="nav-item">
                     <NavLink
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      style={{ border: "none" }}
+                      onClick={handelLogout}
+                      to="/login"
+                      className="nav-link"
                     >
-                      {auth?.user?.name}
+                      Logout
                     </NavLink>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <NavLink
-                          to={`/dashboard/${
-                            auth?.user?.role === 1 ? "admin" : "user"
-                          }`}
-                          className="dropdown-item"
-                        >
-                          Dashboard
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          onClick={handleLogout}
-                          to="/login"
-                          className="dropdown-item"
-                        >
-                          Logout
-                        </NavLink>
-                      </li>
-                    </ul>
                   </li>
                 </>
               )}
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link">
-                  <Badge count={cart?.length} showZero offset={[10, -5]}>
-                    Cart
-                  </Badge>
+                  Cart (0)
                 </NavLink>
               </li>
             </ul>
@@ -224,3 +83,144 @@ const Header = () => {
 };
 
 export default Header;
+
+// import React from "react";
+// import { NavLink, Link } from "react-router-dom";
+// import { useAuth } from "../../context/auth";
+// import toast from "react-hot-toast";
+// import SearchInput from "../Form/SearchInput";
+// import useCategory from "../../hooks/useCategory";
+// import { useCart } from "../../context/cart";
+// import { Badge } from "antd";
+
+// const Header = () => {
+//   const [auth, setAuth] = useAuth();
+//   const [cart] = useCart();
+//   const categories = useCategory();
+//   const handleLogout = () => {
+//     setAuth({
+//       ...auth,
+//       user: null,
+//       token: "",
+//     });
+//     localStorage.removeItem("auth");
+//     toast.success("Logout Successfully");
+//   };
+//   return (
+//     <>
+//       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+//         <div className="container-fluid">
+//           <button
+//             className="navbar-toggler"
+//             type="button"
+//             data-bs-toggle="collapse"
+//             data-bs-target="#navbarTogglerDemo01"
+//             aria-controls="navbarTogglerDemo01"
+//             aria-expanded="false"
+//             aria-label="Toggle navigation"
+//           >
+//             <span className="navbar-toggler-icon" />
+//           </button>
+//           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+//             <Link to="/" className="navbar-brand">
+//               🛒 Ecommerce App
+//             </Link>
+//             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+//               <SearchInput />
+//               <li className="nav-item">
+//                 <NavLink to="/" className="nav-link ">
+//                   Home
+//                 </NavLink>
+//               </li>
+//               <li className="nav-item dropdown">
+//                 <Link
+//                   className="nav-link dropdown-toggle"
+//                   to={"/categories"}
+//                   data-bs-toggle="dropdown"
+//                 >
+//                   Categories
+//                 </Link>
+//                 <ul className="dropdown-menu">
+//                   <li>
+//                     <Link className="dropdown-item" to={"/categories"}>
+//                       All Categories
+//                     </Link>
+//                   </li>
+//                   {categories?.map((c) => (
+//                     <li>
+//                       <Link
+//                         className="dropdown-item"
+//                         to={`/category/${c.slug}`}
+//                       >
+//                         {c.name}
+//                       </Link>
+//                     </li>
+//                   ))}
+//                 </ul>
+//               </li>
+
+//               {!auth?.user ? (
+//                 <>
+//                   <li className="nav-item">
+//                     <NavLink to="/register" className="nav-link">
+//                       Register
+//                     </NavLink>
+//                   </li>
+//                   <li className="nav-item">
+//                     <NavLink to="/login" className="nav-link">
+//                       Login
+//                     </NavLink>
+//                   </li>
+//                 </>
+//               ) : (
+//                 <>
+//                   <li className="nav-item dropdown">
+//                     <NavLink
+//                       className="nav-link dropdown-toggle"
+//                       href="#"
+//                       role="button"
+//                       data-bs-toggle="dropdown"
+//                       style={{ border: "none" }}
+//                     >
+//                       {auth?.user?.name}
+//                     </NavLink>
+//                     <ul className="dropdown-menu">
+//                       <li>
+//                         <NavLink
+//                           to={`/dashboard/${
+//                             auth?.user?.role === 1 ? "admin" : "user"
+//                           }`}
+//                           className="dropdown-item"
+//                         >
+//                           Dashboard
+//                         </NavLink>
+//                       </li>
+//                       <li>
+//                         <NavLink
+//                           onClick={handleLogout}
+//                           to="/login"
+//                           className="dropdown-item"
+//                         >
+//                           Logout
+//                         </NavLink>
+//                       </li>
+//                     </ul>
+//                   </li>
+//                 </>
+//               )}
+//               <li className="nav-item">
+//                 <NavLink to="/cart" className="nav-link">
+//                   <Badge count={cart?.length} showZero offset={[10, -5]}>
+//                     Cart
+//                   </Badge>
+//                 </NavLink>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// };
+
+// export default Header;
