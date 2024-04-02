8e95236f60d8576b59d0aa6a2293392fef4cfa44
Suvrojit <csuvrojit.2002@gmail.com>
2024-04-02 23:38:04 +0530

    Initial commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,29 +7,29 @@ export const registerController = async (req, res) => {
     const { name, email, password, gender, phone, address } = req.body;
     //validations
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
-      return res.send({ error: "Email is Required" });
+      return res.send({ message: "Email is Required" });
     }
     if (!password) {
-      return res.send({ error: "Password is Required" });
-    }
-    if (!gender) {
-      return res.send({ error: "Gender is Required" });
+      return res.send({ message: "Password is Required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone no is Required" });
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!gender) {
+      return res.send({ message: "Gender is Required" });
     }
     if (!address) {
-      return res.send({ error: "Address is Required" });
+      return res.send({ message: "Address is Required" });
     }
     //check user
     const exisitingUser = await userModel.findOne({ email });
     //exisiting user
     if (exisitingUser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "Already Register please login",
       });
     }
@@ -40,8 +40,8 @@ export const registerController = async (req, res) => {
       name,
       email,
       phone,
-      address,
       gender,
+      address,
       password: hashedPassword,
     }).save();
 
